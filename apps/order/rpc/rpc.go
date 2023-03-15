0eaf563e0cee9015f156ee4aa1d389ad29ddf4ba
zhaorx <zhaorenxiang1988@gmail.com>
2023-03-15 17:22:14 +0800

    Initial commit

---
+++ apps/order/rpc/rpc.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"mylebron/apps/order/rpc/internal/config"
+	"mylebron/apps/order/rpc/internal/server"
+	"mylebron/apps/order/rpc/internal/svc"
+	"mylebron/apps/order/rpc/rpc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/rpc.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+	svr := server.NewRpcServer(ctx)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		rpc.RegisterRpcServer(grpcServer, svr)
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
