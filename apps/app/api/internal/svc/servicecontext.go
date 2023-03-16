7ad0b843f7dc30556563dd09a97d97ea2bb3b05a
zhaorx <zhaorenxiang1988@gmail.com>
2023-03-16 14:40:33 +0800

    第三节结束

--- apps/app/api/internal/svc/servicecontext.go
+++ apps/app/api/internal/svc/servicecontext.go
@@ -1,15 +1,22 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"mylebron/apps/app/api/internal/config"
+	"mylebron/apps/order/rpc/order"
+	"mylebron/apps/product/rpc/product"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	OrderRPC   order.Order
+	ProductRPC product.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		OrderRPC:   order.NewOrder(zrpc.MustNewClient(c.OrderRPC)),
+		ProductRPC: product.NewProduct(zrpc.MustNewClient(c.ProductRPC)),
 	}
 }
